eb4dc4df3e5d93cab8fef1793aec9ca3d002e259
Anushikha-00 <anushikha21nov@gmail.com>
2023-07-05 22:14:33 +0530

    commit

--- src/main/java/com/greenStitch/assignment/AssignmentApplication.java
+++ src/main/java/com/greenStitch/assignment/AssignmentApplication.java
@@ -2,8 +2,10 @@ package com.greenStitch.assignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages="com.greenStitch.assignment")
 public class AssignmentApplication {
 
 	public static void main(String[] args) {
